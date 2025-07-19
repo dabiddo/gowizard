ac1517e0dbe167e8abf67babc2b231d739dca6d7
David Torres <dabiddo@gmail.com>
2025-07-19 10:00:29 -0700

    chore: Nuxt 3 and Nodejs 22 (#11)
    * feat: nuxt 3 projects
    
    * chore(node-22): upgrade nodejs to LTS 22
    
    upgrade nodejs to LTS 22

--- internal/projects/nuxt.go
+++ internal/projects/nuxt.go
@@ -62,6 +62,55 @@ func CreateNuxtProject(name string) string {
 	return message
 }
 
+func CreateNuxtThreeProject(name string) string {
+	utils.ClearScreen()
+
+	// Build the Docker command
+	cmd := exec.Command("docker", "run", "--rm",
+		"-v", fmt.Sprintf("%s:/app", utils.GetCurrentPath()),
+		"-w", "/app",
+		"-it",
+		"dabiddo/larabox",
+		"sh", "-c",
+		fmt.Sprintf("pnpm create nuxt %s -t v3 --yes --package-manager pnpm --git-init && chown -R $(id -u):$(id -g) %s", name, name))
+
+	// Set up pipes for real-time output
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+
+	fmt.Println("Creating NuxtJs project...")
+
+	// Run the command
+	if err := cmd.Run(); err != nil {
+		fmt.Printf("\nError creating NuxtJs project: %v\n", err)
+		return "Failed to create NuxtJs project"
+	}
+
+	// Change ownership of the project directory
+	currentUser, err := user.Current()
+	if err != nil {
+		fmt.Printf("\nError getting current user: %v\n", err)
+		return "Failed to get current user"
+	}
+
+	utils.ChangeOwnership(utils.GetCurrentPath(), currentUser.Username, name)
+
+	// Clean up temporary files
+	cleanCmd := exec.Command("rm", "-rf", ".pnpm-store")
+	if err := cleanCmd.Run(); err != nil {
+		fmt.Printf("\nWarning: Failed to clean temporary files: %v\n", err)
+	}
+
+	// Create dev container files
+	utils.CreateDevContainer(name, "_nuxt.stub")
+	utils.CreateDockerfile(name, "_nuxt.stub")
+
+	message := fmt.Sprintf("NuxtJs project '%s' created successfully!\n", name)
+	fmt.Print(message)
+	return message
+}
+
 // Create a new Nuxt.js with MySQL project
 func CreateNuxtWithMySQL(name string) string {
 	utils.ClearScreen()
