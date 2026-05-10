864a5035af7100be7e832a72a4628063e2d37088
David Torres <dabiddo@gmail.com>
2026-05-10 05:29:58 +0000

    feat:new menu WIP

--- cmd/gowizard/main.go
+++ cmd/gowizard/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
-	"flag"
+	"bufio"
 	"fmt"
-	"gowizard/internal/menu"
+	"gowizard/internal/projects"
 	"gowizard/internal/utils"
+	"log"
+	"os"
 
-	"github.com/charmbracelet/lipgloss"
+	"charm.land/huh/v2"
+	"charm.land/lipgloss/v2"
 )
 
 // Styling
@@ -34,25 +37,141 @@ func printBanner() string {
 	return bannerStyle.Render(banner)
 }
 
+type projectType int
+
+const (
+	vanilla projectType = iota
+	PHPFramework
+	JSFramework
+)
+
+func (c projectType) String() string {
+	return [...]string{"Vanilla Project", "PHP Framework", "JS Framework"}[c]
+}
+
 func main() {
+	var category projectType
+	type opts []huh.Option[string]
 
-	// Define command-line flags
-	initFlag := flag.Bool("init", false, "Initialize a new project")
-	wizardFlag := flag.Bool("wizard", false, "Run the wizard interface")
-	flag.Parse()
+	var choice string
+	var projectName string
 
-	var menuBanner string
-	menuBanner += printBanner() + "\n" + utils.AccessPath() + "\n\n"
-	fmt.Printf(menuBanner)
-	// Entry point for the application
+	menuBanner := printBanner() + "\n" + utils.AccessPath() + "\n"
+	// Then ask for a specific food item based on the previous answer.
+	form := huh.NewForm(
 
-	if *initFlag {
-		// Run the init menu
-		menu.InitMenu()
-	} else if *wizardFlag {
-		menu.InitWizard()
-	} else {
-		// Run the regular menu
-		menu.InitDefault()
+		huh.NewGroup(
+
+			huh.NewNote().
+				Title("Welcome!").
+				Description(menuBanner),
+
+			huh.NewInput().
+				Title("Enter Project Name").
+				Prompt("e.j. My Project: ").Value(&projectName),
+
+			huh.NewSelect[projectType]().
+				Title("What Type of Project?").
+				Value(&category).
+				Options(
+					huh.NewOption("Vanilla Project", vanilla),
+					huh.NewOption("PHP Framework Project", PHPFramework),
+					huh.NewOption("JS Framework Project", JSFramework),
+				),
+
+			huh.NewSelect[string]().
+				Value(&choice).
+				Height(7).
+				TitleFunc(func() string {
+					return fmt.Sprintf("Okay, what kind of %s do you want to make?", category)
+				}, &category).
+				OptionsFunc(func() []huh.Option[string] {
+					switch category {
+					case JSFramework:
+						return []huh.Option[string]{
+							huh.NewOption("Vuejs", "vuejs"),
+							huh.NewOption("React", "react"),
+							huh.NewOption("Nuxt 4", "nuxt"),
+							huh.NewOption("Astro", "astro"),
+						}
+					case PHPFramework:
+						return []huh.Option[string]{
+							huh.NewOption("Laravel", "laravel"),
+							huh.NewOption("Laravel CLI", "laravelcli"),
+							huh.NewOption("Laravel Starterkit", "laravelstarterkit"),
+							huh.NewOption("Symfony", "synfony"),
+						}
+					default:
+						return []huh.Option[string]{
+							huh.NewOption("C++", "cpp"),
+							huh.NewOption("Golang", "golang"),
+							huh.NewOption("PHP", "php"),
+							huh.NewOption("Rust", "rust"),
+							huh.NewOption("Zig", "zig"),
+						}
+					}
+				}, &category),
+		),
+	)
+
+	err := form.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	switch choice {
+	case "laravel_composer":
+		projects.CreateLaravelProject(projectName)
+	case "laravel_starter":
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Enter starter Project (larave/react): ")
+		starter, _ := reader.ReadString('\n')
+		projects.CreateLaravelStarterProject(projectName, starter)
+	case "laravel_cli":
+		projects.CreateLaravelCLIProject(projectName)
+	case "laravel_mysql":
+		projects.CreateLaravelWithMySQL(projectName)
+	case "laravel_pgsql":
+		projects.CreateLaravelWithPostgreSQL(projectName)
+	case "nuxt":
+		projects.CreateNuxtProject(projectName)
+	case "nuxt_mysql":
+		projects.CreateNuxtWithMySQL(projectName)
+	case "nuxt_pocketbase":
+		projects.CreateNuxtWithPocketbase(projectName)
+	case "astro_web":
+		projects.CreateAstroProject(projectName)
+	case "astro_blog":
+		projects.CreateAstroBlogProject(projectName)
+	case "refine":
+		projects.CreateRefineProject(projectName)
+	case "better_stack":
+		projects.CreateTanstackProject(projectName)
+	case "nest":
+		projects.CreateNestJSProject(projectName)
+	case "payload_cms":
+		projects.CreatePayloadCMSProject(projectName)
+	case "rust":
+		projects.CreateRustProject(projectName)
+	case "hono":
+		projects.CreateHonoJsProject(projectName)
+	case "hono_openapi":
+		projects.CreateHonoOpenApi(projectName)
+	case "nuxt_3":
+		projects.CreateNuxtThreeProject(projectName)
+	case "cakephp":
+		projects.CreateCakePhpProject(projectName)
+	case "vuejs":
+		projects.CreateVueProject(projectName)
+	case "golang":
+		projects.CreateGolangProject(projectName)
+	case "update":
+		projects.UpdateImages(projectName)
+	case "cpp":
+		projects.CreateCppProject(projectName)
+	case "zig":
+		projects.CreateZigProject(projectName)
+	default:
+		fmt.Println("Invalid option selected.")
 	}
 }
